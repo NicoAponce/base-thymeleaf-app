88ae48f7229ed59cdef53f536da784befe37863f
Nicolas <aponcenicolas@gmail.com>
2020-11-14 20:48:29 -0500

    add basic thymeleaf app

---
+++ src/main/java/spring/project/securitynico/SecurityNicoApplication.java
@@ -0,0 +1,13 @@
+package spring.project.securitynico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityNicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityNicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/project/securitynico/SecurityNicoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.project.securitynico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityNicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
