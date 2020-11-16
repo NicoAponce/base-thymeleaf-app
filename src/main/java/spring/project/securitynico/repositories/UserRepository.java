bff3ea1e6b785c054ab1f7196b18333509bd5952
Nicolas <aponcenicolas@gmail.com>
2020-11-15 19:54:47 -0500

    authorization with roles of database

---
+++ src/main/java/spring/project/securitynico/repositories/UserRepository.java
@@ -0,0 +1,1 @@
+package spring.project.securitynico.repositories;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;import spring.project.securitynico.entities.User;@Repositorypublic interface UserRepository extends JpaRepository<User, Long> {    User findByUsername(String username);}
